ae5b4dcebb263a0ce42210764144caeb0edc380f
eulis01 <eulis01@gmail.com>
2020-07-14 01:35:46 -0400

    added code

--- db/migrate/01_create_artist.rb
+++ db/migrate/01_create_artist.rb
@@ -1,7 +1,10 @@
 class CreateArtists < ActiveRecord:: Migration[5.2]
-  def up 
-  end
-
-  def down
+  def change
+    create_table :artist do |t|
+      t.string :name
+      t.string :genre
+      t.interger :age
+      t.string :hometown
+    end
   end
 end
